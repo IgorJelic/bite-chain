2792e06dd9318a2ed40ba9f19fae21de27fe090d
IgorJelic <jelicigor.98@gmail.com>
2025-07-03 16:04:13 +0200

    Restoraunt service initial commit

---
+++ restorauntservice/src/test/java/com/bitechain/restorauntservice/RestorauntserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitechain.restorauntservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestorauntserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
